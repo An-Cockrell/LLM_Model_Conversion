528f06d643a5db3fc153999e5c40f0f78849162e
dale <daleblarie@gmail.com>
2024-03-14 15:13:34 -0700

    copy example explicitly

--- run_llm_function_call_test.py
+++ run_llm_function_call_test.py
@@ -79,7 +79,6 @@ Edge cases you must handle:
 <|im_start|>assistant"""
     
 
-
     generation_config = model.generation_config
     generation_config.update(
         **{
@@ -97,22 +96,17 @@ Edge cases you must handle:
         }
     )
 
-
-    # input_tokens = tokenizer.apply_chat_template(chat, return_tensors="pt").to(model.device)
-    input_tokens = tokenizer(prompt, return_tensors="pt").to(model.device)
-
     model = model.eval()
-    # querying model
+    inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
+    n_tokens = inputs.input_ids.numel()
+
     with torch.inference_mode():
+        generated_tokens = model.generate(**inputs, generation_config=generation_config)
 
-        outputs = model.generate(input_tokens, pad_token_id=tokenizer.eos_token_id, max_new_tokens=2048, do_sample=True)
-    # getting just the response tokens, no input tokens
-    response = outputs.squeeze()[len(input_tokens[0]):]
-    # convert back to plain text
-    response_plain_text = tokenizer.decode(response, skip_special_tokens=True)
 
-    return response_plain_text
-
+    return tokenizer.decode(
+        generated_tokens.squeeze()[n_tokens:], skip_special_tokens=False
+    )
 
 def extract_function_calls(completion):
     completion = completion.strip()
