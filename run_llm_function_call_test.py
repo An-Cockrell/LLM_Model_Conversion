81450deb4dedda5d2f673ab70d9dfaf3ccdbf77b
Dale Vacc <daleblarie@gmail.com>
2024-03-18 19:55:56 -0400

    refining function calling system

--- run_llm_function_call_test.py
+++ run_llm_function_call_test.py
@@ -9,6 +9,8 @@ from sklearn.metrics.pairwise import cosine_similarity
 import time
 import json
 
+import random
+
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from InstructorEmbedding import INSTRUCTOR
 
@@ -26,79 +28,93 @@ from langchain.chains.openai_functions import convert_to_openai_function
 
 counter = 0
 class test_functions():
-    def BookRecommendation(interest, recommended_book):
+    def BookRecommendation(interest: str) -> str:
+        """Provides book recommendations based on specified interest for a test case."""
         global counter
         counter += 1
-        print("based on your interest in :\n{}".format(interest))
-        print("I think this would be a good book:\n{}".format(recommended_book))
-        return "book recommend return {}".format(counter)
+        print("CALLING BOOK RECOMMENDATION")
 
-    def Joke(setup, punchline):
-        global counter
-        counter += 1
-        print("SETUP:\n{}".format(setup))
-        print("PUNCHLINE:\n{}".format(punchline))
-        return "joke return {}".format(counter)
+        res = "Based on your interest in: {}".format(interest)
+        books = ['Stars Beyond the Void', 'Whispers in the Shadows', 'The Crown of Forgotten Realms']
+        res += "\nI think you would like the book {}".format(random.choice(books))
+        return res + "\ncounter {}".format(counter)
 
-    def SongRecommendation(genre, song):
+    def Joke(setup: str, punchline: str) -> str:
+        """Formats a joke that includes the setup and punchline for a test case"""
         global counter
         counter += 1
-        print("Based on your interest in: {}".format(genre))
-        print("I think you would like:\n{}".format(song))
-        return "song recommend return{}".format(counter)
+        print("CALLING JOKE")
+        res = "Hey do you wanna hear a joke? {}".format(setup)
+        res += punchline.upper() + "\nHAR HAR HAR!!!"
+        # print("SETUP:\n{}".format(setup))
+        # print("PUNCHLINE:\n{}".format(punchline))
+        return res + "\ncounter {}".format(counter)
 
-    def LoadFile(filename):
+    def SongRecommendation(genre: str) -> str:
+        """Provides song recommendations based on specified genre for a test case."""
         global counter
         counter += 1
-        print("LOADING FILE {}".format(filename))
-        return "file load return{}".format(counter)
+        print("CALLING SONG RECOMMENDATION")
+        res = "Based on your interest in: {}".format(genre)
+        songs = ['my horse is getting old', 'I sure love my farm', 'cold beer and blue jeans are all I need']
+        res += "\nI think you would like the song {}".format(random.choice(songs))
+        return res + "\ncounter {}".format(counter)
+
+    def LoadFile(filename: str) -> str:
+        """pretents to load a csv file into a numpy array for a test case and returns a success message."""
+        global counter
+        counter += 1
+        print("CALLING LOAD FILE")
+
+        res = "LOADING FILE {}".format(filename)
+        return res + "\ncounter {}".format(counter)
         
-class BookRecommendation(BaseModel):
-    """Provides book recommendations based on specified interest."""
-    interest: str = Field(description="question of user interest about a book.")
-    recommended_book: str = Field(description="answer to recommend a book")
+# class BookRecommendation(BaseModel):
+#     """Provides book recommendations based on specified interest."""
+#     interest: str = Field(description="question of user interest about a book.")
+#     recommended_book: str = Field(description="answer to recommend a book")
 
-    @validator("interest")
-    def interests_must_not_be_empty(cls, field):
-        if not field:
-            raise ValueError("Interest cannot be empty.")
-        return field
+#     @validator("interest")
+#     def interests_must_not_be_empty(cls, field):
+#         if not field:
+#             raise ValueError("Interest cannot be empty.")
+#         return field
 
-class Joke(BaseModel):
-    """Get a joke that includes the setup and punchline"""
-    setup: str = Field(description="question to set up a joke")
-    punchline: str = Field(description="answer to resolve the joke")
+# class Joke(BaseModel):
+#     """Get a joke that includes the setup and punchline"""
+#     setup: str = Field(description="question to set up a joke")
+#     punchline: str = Field(description="answer to resolve the joke")
 
-    # You can add custom validation logic easily with Pydantic.
-    @validator("setup")
-    def question_ends_with_question_mark(cls, field):
-        if field[-1] != "?":
-            raise ValueError("Badly formed question!")
-        return field
+#     # You can add custom validation logic easily with Pydantic.
+#     @validator("setup")
+#     def question_ends_with_question_mark(cls, field):
+#         if field[-1] != "?":
+#             raise ValueError("Badly formed question!")
+#         return field
     
 
-class SongRecommendation(BaseModel):
-    """Provides song recommendations based on specified genre."""
-    genre: str = Field(description="question to recommend a song.")
-    song: str = Field(description="answer to recommend a song")
+# class SongRecommendation(BaseModel):
+#     """Provides song recommendations based on specified genre."""
+#     genre: str = Field(description="question to recommend a song.")
+#     song: str = Field(description="answer to recommend a song")
 
-    @validator("genre")
-    def genre_must_not_be_empty(cls, field):
-        if not field:
-            raise ValueError("genre cannot be empty.")
-        return field
+#     @validator("genre")
+#     def genre_must_not_be_empty(cls, field):
+#         if not field:
+#             raise ValueError("genre cannot be empty.")
+#         return field
+    
+#     return "asdf"
         
+# class LoadFile(BaseModel):
+#     """Loads a csv file into a numpy array."""
+#     filename: list[str] = Field(description="path of file to load")
 
-
-class LoadFile(BaseModel):
-    """Loads a csv file into a numpy array."""
-    filename: list[str] = Field(description="path of file to load")
-
-    @validator("filename")
-    def filename_must_not_be_empty(cls, field):
-        if not field:
-            raise ValueError("filename cannot be empty.")
-        return field
+#     @validator("filename")
+#     def filename_must_not_be_empty(cls, field):
+#         if not field:
+#             raise ValueError("filename cannot be empty.")
+#         return field
     
 
 def extract_function_calls(completion):
@@ -127,17 +143,19 @@ def extract_function_prompt(completion):
 
     
 def generate_function_call(prompt, model, tokenizer, generation_config_overrides={}):
-    fn = """{"name": "function_name", "arguments": {"arg_1": "value_1", "arg_2": value_2, ...}}"""
+    fn = """{"name": "module_name.function_name", "arguments": {"arg_1": "value_1", "arg_2": value_2, ...}}"""
     prompt = f"""<|im_start|>system
-You are a helpful assistant with access to the following functions:
+You are a helpful assistant with access to the following functions from the 'test_functions' module:
 
-{convert_to_openai_function(Joke)}
+{convert_to_openai_function(test_functions.Joke)}
 
-{convert_to_openai_function(BookRecommendation)}
+{convert_to_openai_function(test_functions.BookRecommendation)}
 
-{convert_to_openai_function(SongRecommendation)}
+{convert_to_openai_function(test_functions.SongRecommendation)}
 
-{convert_to_openai_function(LoadFile)}
+{convert_to_openai_function(test_functions.LoadFile)}
+
+You also have access to all of the functions in the numpy module. Please refer to numpy functions as 'np.function_name'.
 
 To use these functions respond with:
 
@@ -156,7 +174,8 @@ Edge cases you must handle:
 {prompt}<|im_end|>
 <|im_start|>assistant"""
 
-
+    # print("THIS IS THE INPUT PROMPT:\n\n {}".format(prompt))
+    # print("^^^ THAT WAS THE PROMPT")
     model = model.eval()
     inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
     n_tokens = inputs.input_ids.numel()
@@ -171,7 +190,7 @@ Edge cases you must handle:
     return output
 
 
-def generate_response_with_function_calls(prompt, model, tokenizer):
+def generate_response_with_function_calls(input_prompt, model, tokenizer):
     prompt=f"""<|im_start|>system
     You are an expert at generating a natural language prompts 
     for calling specified functions, and if a function is available,
@@ -180,11 +199,17 @@ def generate_response_with_function_calls(prompt, model, tokenizer):
     to generate python code that achieves the desired functinality.
     You are very very CONCISE in your responses.
 
-    Here are functions that can be called:
-        BookRecommendation
-        Joke
-        SongRecommendation
-        LoadFile
+    Here are functions that can be called from the test_functions module:
+{convert_to_openai_function(test_functions.Joke)}
+
+{convert_to_openai_function(test_functions.BookRecommendation)}
+
+{convert_to_openai_function(test_functions.SongRecommendation)}
+
+{convert_to_openai_function(test_functions.LoadFile)}
+
+    You also have access to all functions in the numpy module. 
+        
     
     Here is how you must respond to my messages:
             
@@ -197,30 +222,6 @@ def generate_response_with_function_calls(prompt, model, tokenizer):
     <function_prompt>{{"name":"prompt_for_function_call_2", "prompt": "I am looking for a good joke"}}</function_prompt>,
     ...
 </multiple_prompts>
-                
-    2. When you receive a SUCCESS message with the result from the  
-        tool, you must present the result to me in a nice way (CONCISELY), 
-        and ask: 'What else can I help with?'
-        Example:
-            ME(User): "SUCCESS! result from `I am looking for song recommendations for country music` is ['my horse is getting old', 'I sure love my farm', 'cold beer and blue jeans are all I need']"
-            YOU(Assistant): "If you want song recommendations for country music, here are a few good ones! 
-                1. my horse is getting old"
-                2. I sure love my farm
-                3. cold beer and blue jeans are all I need
-            ME(User): "Thanks, but I don't really like country music that much. Do you know any rock music?"
-            YOU(Assistant): 
-<multiple_prompts>
-    <function_prompt>{{prompt for function call in JSON format}}</function_prompt>
-</multiple_prompts>
-
-    3. When I say there was an error in using the function/tool,
-        you must generate a new prompt that has the same details and meaning, but is worded differently.
-            Example:
-            ME(User): "There was an error in your use of the tool:..."
-            YOU(Assistant):                 
-<multiple_prompts>
-    <function_prompt>{{prompt for function call in JSON format}}</function_prompt>
-</multiple_prompts>
 
     4. If you choose not to generate a prompt, explain why you chose not
         to, and generate a function that achieves the desired functionality.
@@ -232,10 +233,9 @@ def generate_response_with_function_calls(prompt, model, tokenizer):
                     return ((flangbop/10) + 3) * flangbop"
                 '''
 
-    Please respond to the user prompt, using function calls when apropriate. If you prompt a function call, stop and wait for the results before continuing your response.
     <|im_end|>
     <|im_start|>user
-    {prompt}<|im_end|>
+    {input_prompt}<|im_end|>
     <|im_start|>assistant
 """
 
@@ -250,7 +250,8 @@ def generate_response_with_function_calls(prompt, model, tokenizer):
     output = tokenizer.decode(
         generated_tokens.squeeze()[n_tokens:], skip_special_tokens=False
     )
-
+    # print("\n\nPROMPTING MODEL OUTPUT\n\n{}".format(output))
+    # print("END RAW MODEL OUTPUT")
     function_prompts = extract_function_prompt(output)
     results = {}
     if function_prompts:
@@ -261,15 +262,72 @@ def generate_response_with_function_calls(prompt, model, tokenizer):
 
             for function in functions:
                 results[function_prompt["name"]] = function
-                # print("now calling function called test_functions.{}".format(function["name"]))
-                # results[function_prompt["name"]] = getattr(test_functions, function["name"])(**function["arguments"])
+                # print(function["name"])
+                # print(function["name"].split("."[0], 1))
+                # print("now calling function called {}.{}".format(function["name"].split(".", 1)[0], function["name"].split(".", 1)[1]))
+                # results[function_prompt["name"]]["return_value"] = getattr(test_functions, function["name"])(**function["arguments"])
+                results[function_prompt["name"]]["return_value"] = getattr(
+                                                eval(function["name"].split(".", 1)[0]),
+                                                function["name"].split(".", 1)[1])(**function["arguments"])
             # print(results)
     else:
         print("WE DID NOT MAKE A FUNCTION")
 
-
+    # print("THESE ARE THE RESUKLTS{}".format(results))
     # TODO link this to real functions and have the model produce a polished final output
-    return results
+    return_prompt = f"""<|im_start|>system
+    You are a hepful assistant who is good at responding to user queries in a helpful way.
+    You will be given a user prompt, and some function calls and their return values in json format.
+    Please generate a nicely formatted response that solves the user prompt. Respond to ALL parts of the user prompt.
+
+    Here is an example of a desired response format
+    1. When you receive a message with the result from the  
+        tool, you must present the result to me in a nice way (CONCISELY), 
+        and ask: 'What else can I help with?'
+        Example:
+            ME(User): "I am looking for song recommendations for metal music.
+    {{'prompt_for_function_call_1': {{'name': 'test_functions.songRecommendation', 'arguments': {{'genre': 'metal'}}, 'return_value': 'Based on your interest in: metal\nI think you would like the song 'Heavy Iron'}}}}
+            YOU(Assistant): "If you want a song recommendation for metal music, here is a great one! 
+                1. Heavy Iron
+
+    2. If there were multiple functions called, try to combine
+        the results of their outputs into a single unified response.
+        Example:
+            ME(User): <prompt that requires two function calls>
+{{'prompt_for_function_call_1': {{'name': 'function_name_1', 'arguments': {{'arg_name_1': 'arg1', 'arg_name_2': 'arg_2'}}, 'return_value': 'return value 1'}}, 'prompt_for_function_call_2': {{'name': 'function_name_2', 'arguments': {{'arg_name_1': 'arg1', 'arg_name_2': 'arg_2'}}, 'return_value': 'return value 2'}}}}
+            YOU(Assistant): <Response to prompt relating to function call 1>
+{{Response to prompt relating to function call 2}}
+
+    3. If there was an error in using the function/tool, the 
+        user prompt will be followed by an empty set of curly braces {{}}.
+        Inform the user that there was a problem calling a function, and try to give a reason why.
+            Example:
+            ME(User): "I want to know the flibbity transformation of the value flangbob"
+            YOU(Assistant): "I am sorry, but there was a problem trying to generate a function call calling prompt for your input, because I dont have access to a function related to calculating a 'flibbity transformation'. Here is my best interpretation of the function, 'flibbity_transformation
+    '''
+        def flibbity_transformation(flangbop):
+            return ((flangbop/10) + 3) * flangbop"
+    ''' 
+
+    <|im_end|>
+    <|im_start|>user
+    {input_prompt}
+    {results}<|im_end|>
+    <|im_start|>assistant
+    """
+    inputs = tokenizer(return_prompt, return_tensors="pt").to(model.device)
+    n_tokens = inputs.input_ids.numel()
+
+    with torch.inference_mode():
+        generated_tokens = model.generate(**inputs, pad_token_id=tokenizer.eos_token_id, max_new_tokens=2048, do_sample=True)
+
+
+    output = tokenizer.decode(
+        generated_tokens.squeeze()[n_tokens:], skip_special_tokens=False
+    )
+
+
+    return output, results
 
 if __name__=="__main__":
     # mixtral
@@ -296,15 +354,21 @@ if __name__=="__main__":
     # generation_func = partial(generate_function_call, model=model, tokenizer=tokenizer)
 
     prompts = [
-        "give me a fantasy book recommendation and then give me a good joke",
-        "load these files ['test1.csv', 'test2.csv', test6.csv']"
+        "using test_functions, give me a fantasy book recommendation and then give me a good joke",
+        "load these files using the test_functions module ['test1.csv', 'test2.csv', test6.csv']",
+        "Load this text file [./B_Cell_Model/B Cell Abm Netlogo Notes.txt] with numpy, then summarize the text"
     ]
 
-    for prompt in prompts:
-        completion = generate_function_call(prompt, model, tokenizer)
-        functions = extract_function_calls(completion)
 
-        completion = generate_response_with_function_calls(prompt, model, tokenizer)
-        print(completion)
-        for function in completion:
-            getattr(test_functions, function["name"])(**function["arguments"])
+    import time
+    for prompt in prompts:
+        start = time.time()
+        # completion = generate_function_call(prompt, model, tokenizer)
+        # functions = extract_function_calls(completion)
+
+        completion, function_calls = generate_response_with_function_calls(prompt, model, tokenizer)
+        print("TOTAL GENERATION TIME IS {:.2f}".format(time.time() - start))
+        print("THIS IS THE COMPLETED RESPONSE FROM THE MODEL\n\n{}".format(completion))
+        print("THESE ARE THE FUNCTION CALLS")
+        print(function_calls)
+
